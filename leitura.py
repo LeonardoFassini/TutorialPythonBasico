5d66b8d877d770180417793acaec727680823ffd
NinakoLovu <leehtironi@gmail.cm>
2018-09-05 22:05:08 -0300

    Adicionados os primeiros códigos

---
+++ leitura.py
@@ -0,0 +1,4 @@
+a = int(input()) # Lê um valor e converte para inteiro, salvando em uma variável.
+b = int(input()) # Le outro valor e converte para inteiro, salvando em uma variável.
+resposta = a + b # Realiza a soma desses valores e salva em uma varíavel.
+print(resposta)  # Imprime a variável que contém a resposta.
